8a8a2189882dc5c946f3082113c2f23c5c52308b
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-12-14 14:24:06 -0600

    add packages output

--- flake.nix
+++ flake.nix
@@ -2,16 +2,35 @@
   description = "my custom nix derivations";
 
   inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    nix-config = {
+      url = "github:EarthGman/nix-config";
+    };
+
     fonts = {
       url = "https://raw.githubusercontent.com/EarthGman/assets/master/fonts.json";
       flake = false;
     };
   };
 
-  outputs = { ... } @ inputs: {
-    overlay = rec {
-      packages = final: prev: import ./pkgs { pkgs = final; inherit inputs; };
-      default = packages;
+  outputs = { nixpkgs, nix-config, ... } @ inputs:
+    let
+      lib = nix-config.lib;
+      inherit (lib) forAllSystems;
+    in
+    {
+      overlay = rec {
+        packages = final: prev: import ./pkgs { pkgs = final; inherit inputs; };
+        default = packages;
+      };
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        in
+        import ./pkgs { inherit pkgs inputs; }
+      );
     };
-  };
 }
