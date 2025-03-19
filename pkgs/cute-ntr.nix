2f8c83a7a98a2d4bc99a524ebcd9ac85846c5ed1
EarthGman <117403037+EarthGman@users.noreply.github.com>
2025-03-18 23:50:45 -0500

    add icon

--- pkgs/cute-ntr.nix
+++ pkgs/cute-ntr.nix
@@ -1,9 +1,25 @@
 { stdenv
 , libsForQt5
 , fetchFromGitLab
+, makeDesktopItem
+, fetchurl
+, imagemagick
 , ...
 }:
+let
+  icon = fetchurl {
+    url = "https://gitlab.com/BoltsJ/cuteNTR/-/blob/d217aae4f5708b72c5229174cd53556eea107468/setup/gui/com.gitlab.BoltsJ.cuteNTR.svg";
+    sha256 = "sha256-bnSNJh13E2U11b+qW0SjfvZQ/VQi5Dbuz65g3svTgWo=";
+  };
 
+  desktopItem = makeDesktopItem {
+    name = "cuteNTR";
+    desktopName = "cuteNTR";
+    icon = "cutentr";
+    exec = "cutentr";
+    categories = [ "Game" ];
+  };
+in
 stdenv.mkDerivation {
   pname = "cuteNTR";
   version = "0.3.1";
@@ -20,13 +36,27 @@ stdenv.mkDerivation {
     wrapQtAppsHook
   ];
 
+  nativeBuildInputs = [
+    imagemagick
+  ];
+
   buildPhase = ''
     qmake
     make
   '';
 
   installPhase = ''
+    runHook preinstall
     mkdir -p $out/bin
     cp -r cutentr $out/bin
+
+    install -m 444 -D "${desktopItem}/share/applications/"* \
+      -t $out/share/applications/
+
+    for size in 16 24 32 48 64 128 256 512; do
+      mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
+      magick -background none ${icon} -resize "$size"x"$size" $out/share/icons/hicolor/"$size"x"$size"/apps/cutentr.svg
+    done
+    runHook postInstall
   '';
 }
