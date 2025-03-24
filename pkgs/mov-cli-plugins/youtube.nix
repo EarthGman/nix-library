c5be656c6b1096c605d9205e95314417c3ae7c57
EarthGman <117403037+EarthGman@users.noreply.github.com>
2025-03-24 03:17:48 -0500

    oh wait that builds now

--- pkgs/mov-cli-plugins/youtube.nix
+++ pkgs/mov-cli-plugins/youtube.nix
@@ -1,20 +1,14 @@
-{ pkgs, ... }:
-let
-  python3Packages = pkgs.python3Packages.override {
-    overrides = final: prev: {
-      pytubefix = prev.pytubefix.overridePythonAttrs (old: {
-        doCheck = false; # test fails to resolve domain name lol
-      });
-    };
-  };
-in
+{ python3Packages
+, fetchFromGitHub
+, ...
+}:
 
 python3Packages.buildPythonPackage {
   pname = "mov-cli-youtube";
   version = "1.3.5";
   pyproject = true;
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "mov-cli";
     repo = "mov-cli-youtube";
     rev = "9040c1276af08d25d6977aa33a8a408f7d733324";
